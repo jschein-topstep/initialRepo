87611fc87c463ba8aa850f71dc576ddb4a6fa713
rscheinTS <rschein@topstepllc.com>
2026-05-26 14:42:49 -0500

    initial error handling build

---
+++ tslib/tslib-sppErrorHandling/getErrorCodeSPP.mjs
@@ -0,0 +1,24 @@
+import {
+  DynamoDBClient,
+  GetItemCommand,
+  PutItemCommand,
+} from "@aws-sdk/client-dynamodb";
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
+
+const dynamo = new DynamoDBClient({
+  region: process.env.AWS_REGION || "us-east-1",
+});
+
+export async function getError(errorCode) {
+  const res = await dynamo.send(
+    new GetItemCommand({
+      TableName: sppErrorCodes,
+      Key: marshall({ pk: errorCode }),
+    }),
+  );
+
+  if (!res.Item) {
+    throw new Error(`No error found for code: ${errorCode}`);
+  }
+  return unmarshall(res.Item);
+}
